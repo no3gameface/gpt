72d3e4f9753c005281b7936248ee4f4d07dbd112
no3gameface <122880688+no3gameface@users.noreply.github.com>
2023-08-21 12:44:47 +0100

    Update Auto Quest.lua

--- Auto Quest.lua
+++ Auto Quest.lua
@@ -1,13 +1,3 @@
-function fact(b)
-    if b == 0 then
-        return 1
-    else
-        return b * fact(b - 1)
-    end
-end
-print("enter a number:")
-a = io.read("*number")
-print(fact(a))
 if _G.AutoQuestToggle then
     wait()
     local P = game:GetService("Players")
