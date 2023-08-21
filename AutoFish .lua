49688c16f0b4eb7a35cca535e61ae36b17ae6971
no3gameface <122880688+no3gameface@users.noreply.github.com>
2023-08-21 02:50:22 +0100

    Create AutoFish .lua

---
+++ AutoFish .lua
@@ -0,0 +1,49 @@
+if _G.AutoFishToggle then
+
+
+if game.PlaceId == 5902977746 then
+    local P = game:GetService("Players")
+    local RS = game:GetService("ReplicatedStorage")
+
+    local player = game:GetService("Players").LocalPlayer
+    local targetText = "100/100"
+    local targetCount = tonumber(targetText:match("(%d+)/%d+"))
+
+    local function checkText()
+        local hud = player.PlayerGui.MainGui.HUD
+        local catchText = hud["CatchesToday"].Text.Text
+        local currentCount = tonumber(catchText:match("(%d+)/%d+"))
+
+        local difference = targetCount - currentCount
+
+        if currentCount and targetCount then
+            if catchText:find(targetText, 1, true) then
+                print("Text contains", targetText)
+            else
+                print("Text does not contain", targetText)
+            end
+        else
+            print("Unable to extract counts from text")
+        end
+
+        return difference
+    end
+
+    local calculatedDifference = checkText()
+
+    repeat wait() until P.LocalPlayer
+    repeat wait() until P.LocalPlayer.Character
+    repeat wait() until P.LocalPlayer.Character.HumanoidRootPart
+
+    -- variable
+    local stuff = getrenv()._G.FireNetwork
+    local id = game.Players.LocalPlayer.UserId
+
+    -- hacker stuff
+    for i = 1, calculatedDifference do
+	
+        stuff("PlayerCatchFish", id)
+        wait(4)
+    end
+end
+end
