7802b356a23f89ff1c99691bdb17d076a8a7cd97
no3gameface <122880688+no3gameface@users.noreply.github.com>
2023-08-21 12:38:31 +0100

    Update Auto Quest.lua

--- Auto Quest.lua
+++ Auto Quest.lua
@@ -1,27 +1,23 @@
-wait()
 local P = game:GetService("Players")
 local RS = game:GetService("ReplicatedStorage")
-
 repeat wait() until P.LocalPlayer
 repeat wait() until P.LocalPlayer.Character
 repeat wait() until P.LocalPlayer.Character.HumanoidRootPart
+wait()
 
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local player = game:GetService("Players").LocalPlayer
 local questsFolder = player.PlayerGui.MainGui.MainFrames.Quests.Content
 local GlobalInit = ReplicatedStorage.Modules.GlobalInit
-
 local function getQuestData()
     local questData = {}
-
     for _, quest in pairs(questsFolder:GetChildren()) do
         if quest:IsA("ImageLabel") then
             local titleLabel = quest:FindFirstChild("Title")
             local percentageLabel = quest:FindFirstChild("Percentage")
             local rewardLabel = quest:FindFirstChild("Reward")
             local descriptionLabel = quest:FindFirstChild("Description")
-
             if titleLabel and percentageLabel and rewardLabel and descriptionLabel then
                 local questTitle = titleLabel.Text
                 local questPercentage = tonumber(percentageLabel.Text:match("%d+"))
@@ -30,27 +26,22 @@ local function getQuestData()
                 end
                 local questReward = rewardLabel.Text
                 local questDescription = descriptionLabel.Text:match("%d+")
-
                 table.insert(questData, {id = quest.Name, title = questTitle, percentage = questPercentage, reward = questReward, description = questDescription})
             end
         end
     end
-
     return questData
 end
-
 local function createUI()
     local screenGui = Instance.new("ScreenGui")
     screenGui.Name = "QuestProgressUI"
     screenGui.Parent = game.CoreGui
-
     local frame = Instance.new("Frame")
     frame.Size = UDim2.new(0, 200, 0, 300)
     frame.Position = UDim2.new(1, -220, 0.5, -150)
     frame.BackgroundColor3 = Color3.new(1, 1, 1)
     frame.BorderSizePixel = 0
     frame.Parent = screenGui
-
     local borderGradient = Instance.new("UIGradient")
     borderGradient.Rotation = 0
     borderGradient.Color = ColorSequence.new{
@@ -61,11 +52,9 @@ local function createUI()
         ColorSequenceKeypoint.new(1, Color3.fromRGB(188, 239, 255))
     }
     borderGradient.Parent = frame
-
     local squareCorner = Instance.new("UICorner")
     squareCorner.CornerRadius = UDim.new(0.1, 0)
     squareCorner.Parent = frame
-
     local innerFrame = Instance.new("Frame")
     innerFrame.Size = UDim2.new(0.98, 0, 0.98, 0)
     innerFrame.Position = UDim2.new(0.01, 0, 0.01, 0)
@@ -73,20 +62,16 @@ local function createUI()
     
     innerFrame.BackgroundColor3 = Color3.new(0.2, 0.2, 0.2)
     innerFrame.Parent = frame
-
     local innerCorner = Instance.new("UICorner")
     innerCorner.CornerRadius = UDim.new(0.1, 0)
     innerCorner.Parent = innerFrame
-
     local currentRot = borderGradient.Rotation
     local tweenService = game:GetService("TweenService")
     local tweenInformation = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, -1, false)
     local goal = {Rotation = currentRot + 360}
     local tween = tweenService:Create(borderGradient, tweenInformation, goal)
     tween:Play()
-
     local questData = getQuestData()
-
     local yOffset = 0
     for _, quest in ipairs(questData) do
         local questLabel = Instance.new("TextLabel")
@@ -97,14 +82,10 @@ local function createUI()
         questLabel.TextWrapped = true
         questLabel.TextColor3 = Color3.new(1, 1, 1)
         questLabel.Parent = innerFrame
-
         yOffset = yOffset + 70
     end
 end
-
 createUI()
-
-
 local function autoClaimQuests(questData)
     for _, quest in ipairs(questData) do
         if quest.percentage == 100 then
@@ -112,7 +93,6 @@ local function autoClaimQuests(questData)
         end
     end
 end
-
 local function isQuestsFolderEmpty()
     for _, child in ipairs(questsFolder:GetChildren()) do
         if not child:IsA("UIListLayout") then
@@ -121,14 +101,11 @@ local function isQuestsFolderEmpty()
     end
     return true
 end
-
 local function refreshQuestsIfEmpty()
     if isQuestsFolderEmpty() then
-        print("Refreshing quests")
         GlobalInit.RemoteEvents.PlayerRefreshQuests:FireServer()
     end
 end
-
 while true do
     local questData = getQuestData()
     autoClaimQuests(questData)
