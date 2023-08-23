0c6ac75b65ace02e5f5efc7c506d683c62be48d7
no3gameface <122880688+no3gameface@users.noreply.github.com>
2023-08-23 21:38:15 +0100

    Update AutoFish .lua

--- AutoFish .lua
+++ AutoFish .lua
@@ -1,49 +1,62 @@
-
 if _G.AutoFishToggle then
+    print("AutoFishToggle is enabled")
 
-if game.PlaceId == 5902977746 then
-    local P = game:GetService("Players")
-    local RS = game:GetService("ReplicatedStorage")
+    if game.PlaceId == 5902977746 then
+        print("PlaceId is correct")
 
-    local player = game:GetService("Players").LocalPlayer
-    local targetText = "100/100"
-    local targetCount = tonumber(targetText:match("(%d+)/%d+"))
+        local globalEnv = getrenv()._G
+        if globalEnv.serverType == "Lobby" then
+            print("Server type is Lobby, proceeding...")
 
-    local function checkText()
-        local hud = player.PlayerGui.MainGui.HUD
-        local catchText = hud["CatchesToday"].Text.Text
-        local currentCount = tonumber(catchText:match("(%d+)/%d+"))
+            local P = game:GetService("Players")
+            local RS = game:GetService("ReplicatedStorage")
 
-        local difference = targetCount - currentCount
+            local player = game:GetService("Players").LocalPlayer
+            local targetText = "100/100"
+            local targetCount = tonumber(targetText:match("(%d+)/%d+"))
 
-        if currentCount and targetCount then
-            if catchText:find(targetText, 1, true) then
-                print("Text contains", targetText)
-            else
-                print("Text does not contain", targetText)
+            local function checkText()
+                local hud = player.PlayerGui.MainGui.HUD
+                local catchText = hud["CatchesToday"].Text.Text
+                local currentCount = tonumber(catchText:match("(%d+)/%d+"))
+
+                local difference = targetCount - currentCount
+
+                if currentCount and targetCount then
+                    if catchText:find(targetText, 1, true) then
+                        print("Text contains", targetText)
+                    else
+                        print("Text does not contain", targetText)
+                    end
+                else
+                    print("Unable to extract counts from text")
+                end
+
+                return difference
+            end
+
+            local calculatedDifference = checkText()
+
+            -- Wait for the game to load properly
+            repeat wait() until game:IsLoaded()
+            print("Game has loaded")
+
+            repeat wait() until P.LocalPlayer
+            repeat wait() until P.LocalPlayer.Character
+            repeat wait() until P.LocalPlayer.Character.HumanoidRootPart
+
+            -- variable
+            local stuff = getrenv()._G.FireNetwork
+            local id = game.Players.LocalPlayer.UserId
+
+            -- hacker stuff
+            for i = calculatedDifference, 1, -1 do
+                print(i)
+                stuff("PlayerCatchFish", id)
+                wait(10.5)
             end
         else
-            print("Unable to extract counts from text")
+            print("Server type is not Lobby, exiting...")
         end
-
-        return difference
     end
-
-    local calculatedDifference = checkText()
-
-    repeat wait() until P.LocalPlayer
-    repeat wait() until P.LocalPlayer.Character
-    repeat wait() until P.LocalPlayer.Character.HumanoidRootPart
-
-    -- variable
-    local stuff = getrenv()._G.FireNetwork
-    local id = game.Players.LocalPlayer.UserId
-
-    -- hacker stuff
-for i = calculatedDifference, 1, -1 do
-    print(i)
-    stuff("PlayerCatchFish", id)
-    wait(10.1)
-end
-end
 end
