31981e63c232cd20d5453019ce54c6980809608f
no3gameface <122880688+no3gameface@users.noreply.github.com>
2023-08-23 22:33:12 +0100

    Update MapFixer.lua

--- MapFixer.lua
+++ MapFixer.lua
@@ -1,44 +1,48 @@
-if _G.MapFixerToggle then
--- Wait for the local character to exist
-local player = game:GetService("Players").LocalPlayer
-repeat wait() until player.Character
-print("Local character exists")
+if game.PlaceId == 5902977746 then
+    if _G.MapFixerToggle then
+        -- Wait for the local character to exist
+        local player = game:GetService("Players").LocalPlayer
+        repeat wait() until player.Character
+        print("Local character exists")
 
-local globalEnv = getrenv()._G
-if globalEnv.serverType == "Match" then
-    print("Server type is Match, proceeding...")
+        local globalEnv = getrenv()._G
+        if globalEnv.serverType == "Match" then
+            print("Server type is Match, proceeding...")
 
-    local mapPath = workspace:FindFirstChild("Map")
-    if mapPath and mapPath:FindFirstChild("Path") then
-        print("workspace.Map.Path exists, proceeding...")
+            local mapPath = workspace:FindFirstChild("Map")
+            if mapPath and mapPath:FindFirstChild("Path") then
+                print("workspace.Map.Path exists, proceeding...")
 
-        -- Clone both children under workspace.Map.Path
-        local children = mapPath.Path:GetChildren()
-        for _, child in pairs(children) do
-            local clone = child:Clone()
-            clone.Name = "ForceField" -- Change name
-            clone.Material = Enum.Material.ForceField -- Change material
-            clone.Parent = workspace -- Move clone to workspace
+                -- Clone both children under workspace.Map.Path
+                local children = mapPath.Path:GetChildren()
+                for _, child in pairs(children) do
+                    local clone = child:Clone()
+                    clone.Name = "ForceField" -- Change name
+                    clone.Material = Enum.Material.ForceField -- Change material
+                    clone.Parent = workspace -- Move clone to workspace
+                end
+
+                -- Delete the children under workspace.Map
+                for _, child in pairs(mapPath:GetChildren()) do
+                    child:Destroy()
+                end
+
+                print("Children under workspace.Map.Path cloned and original children under workspace.Map deleted")
+            else
+                print("workspace.Map.Path does not exist, exiting...")
+            end
+
+            -- Enable auto skip
+            local RS = game:GetService("ReplicatedStorage")
+            RS.Modules.GlobalInit.RemoteEvents:WaitForChild("PlayerReadyForNextWave")
+            while wait() do
+                RS.Modules.GlobalInit.RemoteEvents.PlayerReadyForNextWave:FireServer()
+                print("Firing PlayerReadyForNextWave event") -- Debugging print
+            end
+        else
+            print("Server type is not Match, exiting...")
         end
-
-        -- Delete the children under workspace.Map
-        for _, child in pairs(mapPath:GetChildren()) do
-            child:Destroy()
-        end
-
-        print("Children under workspace.Map.Path cloned and original children under workspace.Map deleted")
-    else
-        print("workspace.Map.Path does not exist, exiting...")
-    end
-
-    -- Enable auto skip
-    local RS = game:GetService("ReplicatedStorage")
-    RS.Modules.GlobalInit.RemoteEvents:WaitForChild("PlayerReadyForNextWave")
-    while wait() do
-        RS.Modules.GlobalInit.RemoteEvents.PlayerReadyForNextWave:FireServer()
-        print("Firing PlayerReadyForNextWave event") -- Debugging print
     end
 else
-    print("Server type is not Match, exiting...")
-end
+    print("PlaceId is not 5902977746, exiting...")
 end
